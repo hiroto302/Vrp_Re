6e6dd19e1deea23e632aeadb392cb1ae21b98c4c
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-20 13:30:24 +0900

    Cat(回転・アニメーション)

---
+++ Assets/Scripts/Cat/Cat.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    public enum State
+    {
+        Normal,
+        Talk
+    }
+    public State currentState;
+    float rotationSpeed = 1.0f; // 振り向き速度
+    [SerializeField]
+    Transform playerTransform = null; // 振り向く方向・相手(Player)
+    [SerializeField]
+    PlayerController playerController = null; // playerのスクリプト
+
+    Animator animator; // animation
+    float elapsedTime = 0; // 経過時間
+    float currentDirection; // 現在の向き
+    float nextDirection; // 一定時間経過後に、向いてる方向
+
+    void Reset()
+    {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        currentState = State.Normal;
+    }
+    void Update()
+    {
+        // 猫の状態をPlayerの状態と同様にする
+        if(playerController.currentState == PlayerController.State.Normal && currentState != State.Normal)
+        {
+            SetState(State.Normal);
+        }
+        else if(playerController.currentState == PlayerController.State.Talk && currentState != State.Talk)
+        {
+            SetState(State.Talk);
+        }
+        // Talk状態に移行する時の処理
+        if(currentState == State.Talk)
+        {
+            // Playerの方向に向かせる
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z) - transform.position), rotationSpeed * Time.deltaTime);
+
+            // 振り向きのアニメーション制御
+            if(elapsedTime < 0.2f)
+            {
+                currentDirection = transform.localEulerAngles.y;
+            }
+            elapsedTime += Time.deltaTime;
+            if(elapsedTime > 0.2f)
+            {
+                nextDirection = transform.localEulerAngles.y;
+                elapsedTime = 0;
+            }
+            if(Mathf.Abs(currentDirection -nextDirection) > 0.05f)
+            {
+                animator.SetBool("TurnAround", true);
+            }
+            else
+            {
+                animator.SetBool("TurnAround", false);
+            }
+        }
+
+    }
+    public void SetState(State state)
+    {
+        currentState = state;
+        // animation処理
+        if(state == State.Normal)
+        {
+            animator.SetBool("Talk", false);
+        }
+        else if(state == State.Talk)
+        {
+            animator.SetBool("Talk", true);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/StudyTransform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudyTransform : MonoBehaviour
+{
+    Transform myTransform;
+    /* 5.1
+    Tilt the player using the R keys. When the R keys are released
+    the player will be rotated back to the center using Slerp.
+    */
+    float smooth = 4.0f;     // 回転速度
+    float tiltAngle = 60.0f; // 傾斜角
+    /* 6.1
+    90度回転しているplayerを猫がいる方向に向かせる
+    */
+    float rotationSpeed = 1.0f;
+    Transform catTransform;
+    void Start()
+    {
+        // myTransform = gameObject.GetComponent<Transform>();
+        // myTransform.eulerAngles += new Vector3(0, 10.0f, 0);     3.ワールド座標のy軸を基準に10度回転
+        transform.Rotate(0, 90.0f, 0, Space.World);            // 4.ワールド座標を基準とした回転
+        catTransform = GameObject.Find("Cat").GetComponent<Transform>();
+
+    }
+    void Update()
+    {
+        // Debug.Log(myTransform.eulerAngles);        1.ワールド座標を基準にした角度取得
+        // Debug.Log(myTransform.localEulerAngles);   2.ローカル座標を基準にした角度取得
+
+        //5.2 Smoothly tilts a transform towards a target rotation.
+        // float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        // float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+
+        //5.3 Rotate the cube by converting the angles into a quaternion.
+        // Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+        //5.4 Dampen towards the target rotation
+        // transform.rotation = Quaternion.Slerp(transform.rotation, target,  Time.deltaTime * smooth);
+
+        //6.2
+        // transform.rotation = Quaternion.Slerp(transform.rotation, catTransform.rotation, rotationSpeed * Time.deltaTime); //第一引数 開始位置(from), 第二引数 終わりの位置(to), 第三引数 現在の位置(0~1)
+        // transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(new Vector3(catTransform.position.x, transform.position.y, catTransform.position.z) - transform.position), rotationSpeed * Time.deltaTime);
+    }
+}
